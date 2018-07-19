a27db7d8dd992e13c9cf481f52cff243bbc479b5
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2018-07-19 11:31:15 +0200

    Adding front folder

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="ui">
+    <drawable></drawable>
+  </div>
+</template>
+
+<script>
+import Drawable from './components/Drawable'
+
+export default {
+  name: 'app',
+  components: {
+    Drawable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
---
+++ front/src/components/Drawable.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="flex">
+
+    <div>
+      <h2>Canvas properties</h2>
+      <div>
+        <h3>Actions:</h3>
+        <button @click="submitPoints">submit points</button>
+        <button @click="clearPoints">clear points</button>
+      </div>
+
+      <div>
+        <h3>Size:</h3>
+        <ul>
+          <li>
+            <label>Width: </label>
+            <input type="text" v-model="settings.canvasWidth">
+          </li>
+          <li>
+            <label>Height: </label>
+            <input type="text" v-model="settings.canvasHeight">
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Points properties:</h3>
+        <label>Point Size</label>
+        <input type="number" v-model.number="settings.pointSize">
+      </div>
+    </div>
+
+    <div>
+      <canvas
+        ref="inputCanvas"
+        :width="settings.canvasWidth"
+        :height="settings.canvasHeight"
+        v-insert-points="{points: points, settings: settings}"
+
+        @mousedown="mousedown"
+        @mouseup="mouseup"
+        @mousemove="mousemove"
+      ></canvas>
+    </div>
+
+    <div>
+      <canvas
+        ref="outputCanvas"
+        :width="settings.canvasWidth"
+        :height="settings.canvasHeight"
+        v-insert-sides="{points: resultPoints, settings: settings}"
+      ></canvas>
+    </div>
+
+    <div>
+      <h3>Points (data)</h3>
+      <table  class="ui selectable table">
+        <thead>
+          <tr><th>N°</th><th>X</th><th>Y</th><th>Remove</th></tr>
+        </thead>
+        <tbody>
+          <tr v-for="(point, index) in points" :key="point">
+
+            <td>{{index + 1}}</td>
+            <td><input v-model.number="point.x" type="number"/></td>
+            <td><input v-model.number="point.y" type="number"/></td>
+            <td><button @click="removePoint(index)">X</button></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.canvas = this.$refs.inputCanvas
+    this.context = this.canvas.getContext('2d')
+  },
+  data () {
+    return {
+      canvas: {},
+      context: {},
+      settings: {
+        canvasHeight: 350,
+        canvasWidth: 450,
+        pointSize: 2,
+        strokeStyle: 'darkgrey',
+        fillStyle: 'rgb(200,0,0)'
+      },
+      dragMode: false,
+      overPoint: {},
+      points: [],
+      resultPoints: []
+    }
+  },
+  computed: {
+
+  },
+  directives: {
+    insertPoints: function (canvasElement, binding) {
+      const points = JSON.parse(JSON.stringify(binding.value.points))
+      const settings = binding.value.settings
+      const context = canvasElement.getContext('2d')
+      context.clearRect(0, 0, settings.canvasWidth, settings.canvasHeight)
+
+      for (let point of points) {
+        context.beginPath()
+        context.moveTo(point.x - settings.pointSize, point.y - settings.pointSize)
+        context.lineTo(point.x - settings.pointSize, point.y + settings.pointSize)
+        context.lineTo(point.x + settings.pointSize, point.y + settings.pointSize)
+        context.lineTo(point.x + settings.pointSize, point.y - settings.pointSize)
+        context.lineTo(point.x - settings.pointSize, point.y - settings.pointSize)
+        context.fillStyle = settings.fillStyle
+        context.fill()
+        context.strokeStyle = settings.strokeStyle
+        context.stroke()
+      }
+    },
+    insertSides: (canvasElement, binding) => {
+      const points = JSON.parse(JSON.stringify(binding.value.points))
+      if (!points[0]) {
+        return
+      }
+      const settings = binding.value.settings
+      const context = canvasElement.getContext('2d')
+      context.clearRect(0, 0, settings.canvasWidth, settings.canvasHeight)
+
+      context.beginPath()
+      context.moveTo(points[0][0], points[0][1])
+      for (let point of points.slice(1)) {
+        context.lineTo(point[0], point[1])
+      }
+      context.closePath()
+      context.fillStyle = settings.fillStyle
+      context.fill()
+      context.strokeStyle = settings.strokeStyle
+      context.stroke()
+    }
+  },
+  methods: {
+    removePoint (index) {
+      this.points.splice(index, 1)
+    },
+    matchPoints (checkPoint, mousePoint) {
+      if (mousePoint.x > checkPoint.x + this.settings.pointSize) {
+        return false
+      }
+
+      if (mousePoint.x < checkPoint.x - this.settings.pointSize) {
+        return false
+      }
+
+      if (mousePoint.y > checkPoint.y + this.settings.pointSize) {
+        return false
+      }
+
+      if (mousePoint.y < checkPoint.y - this.settings.pointSize) {
+        return false
+      }
+      return true
+    },
+
+    submitPoints () {
+      const payload = {
+        points: this.points
+      }
+      this.$http.post('http://127.0.0.1:5000/', payload).then((response) => {
+        const uuid = response.data.uuid
+        const ws = new WebSocket('ws://127.0.0.1:5000/polygons/' + uuid)
+        ws.onmessage = (event) => {
+          this.resultPoints = JSON.parse(event.data).vertex
+        }
+      })
+    },
+    clearPoints () {
+      this.points = []
+    },
+    getMousePos (evt) {
+      // https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+      var rect = this.context.canvas.getBoundingClientRect()
+      return {
+        x: parseInt((evt.clientX - rect.left) / (rect.right - rect.left) * this.settings.canvasWidth),
+        y: parseInt((evt.clientY - rect.top) / (rect.bottom - rect.top) * this.settings.canvasHeight)
+      }
+    },
+    mouseup (event) {
+      let mouseUpPoint = this.getMousePos(event)
+      if (this.dragMode && this.overPoint) {
+        this.dragPoint(this.overPoint, mouseUpPoint)
+        this.dragMode = false
+      } else {
+        this.addNode(mouseUpPoint)
+      }
+    },
+    dragPoint (selectedPoint, newPoint) {
+      this.points.map((pointToMove) => {
+        if (pointToMove.x === selectedPoint.x && pointToMove.y === selectedPoint.y) {
+          pointToMove.x = newPoint.x
+          pointToMove.y = newPoint.y
+        }
+      })
+    },
+    mousemove (event) {
+      let point = this.getMousePos(event)
+      // this.selectedPoint(point)
+      if (this.dragMode && this.overPoint) {
+        this.dragPoint(this.overPoint, point)
+      }
+    },
+    mousedown (event) {
+      let point = this.getMousePos(event)
+      if (this.isMouseOnNode(point)) {
+        // point selected
+        this.dragMode = true
+      } else {
+        this.dragMode = false
+      }
+    },
+    isMouseOnNode (point) {
+      return this.selectedPoint(point) !== null
+    },
+    selectedPoint (point) {
+      for (var iteratedPoint of this.points) {
+        if (this.matchPoints(iteratedPoint, point)) {
+          this.overPoint = iteratedPoint
+          return iteratedPoint
+        }
+      }
+      this.overPoint = null
+      return null
+    },
+    addNode (point) {
+      this.points.push(point)
+    }
+  }
+}
+</script>
+<style>
+  input {
+    width: 50px;
+  }
+  canvas {
+    border:1px solid #bbb;
+  }
+  .flex{
+    display: flex;
+    flex-flow: row wrap;
+  }
+  .flex > * {
+    border: 1px solid #eee;
+    margin: 10px;
+    padding: 10px;
+  }
+</style>
