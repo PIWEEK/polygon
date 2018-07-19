7e75def33a33b11c67fd26e370c364496670e4a8
Pablo Ruiz-Muzquiz <diacritica@gmail.com>
2018-07-19 09:57:04 +0200

    simple polygon generator polygongenerator.py

---
+++ polygon/polygongenerator.py
@@ -0,0 +1,43 @@
+import sys
+import polygon
+import spiral
+
+
+def generate(cycles, vertexlist):
+    uniquepolygons = []
+
+    for i in range(cycles):
+        ps = polygon.PolygonStruct(vertexlist)
+        ps.setInitialVertex()
+
+        while not ps.allPointsUsed():
+            ps.cycle()
+            if ps.stuck:
+                break
+
+        if ps.checkValidFinalPolygon():
+            ps.lov.append(ps.lov[0])
+            psreverse = ps.lov[:]
+            psreverse.reverse()
+
+            if ps.lov not in uniquepolygons and psreverse not in uniquepolygons:
+                if ps.lov < psreverse:
+                    uniquepolygons.append(ps.lov)
+                else:
+                    uniquepolygons.append(psreverse)
+
+                yield ps.getJSON()
+
+
+if __name__=="__main__":
+
+    cycles = 500
+    try:
+        steps = int(sys.argv[1])
+    except:
+        steps = 10
+
+    g = spiral.Spiral(xzero=1,yzero=1)
+    vertexlist = list(set(g.generate(steps=steps)))
+    for p in generate(cycles, vertexlist):
+        print(p)
